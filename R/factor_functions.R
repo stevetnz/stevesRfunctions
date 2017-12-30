8c718d404e393409042aa8719fffc83a9d856af2
Steve Taylor <steve.taylor.nz@gmail.com>
2017-12-30 18:30:07 +1300

    Removing some files

--- R/factor_functions.R
+++ R/factor_functions.R
@@ -1,10 +1,9 @@
-c.factor = function(fac1, fac2) {
-  factor(
-    c(as.character(fac1), as.character(fac2)),
-    levels=unique(c(levels(fac1), levels(fac2)))
-  )
-}
-
+#' Convert a logical vector to a factor
+#'
+#' @usage NoYes(bool)
+#' @param bool A logical vector, or a vector that can be coerced to logical.
+#' @return A factor with levels combining the unique levels of the input factors.
+#' @export
 NoYes = function(bool) {
   if(!is.logical(bool)) bool = as.logical(bool)
   factor(ifelse(bool, "Yes", "No"), levels=c("No", "Yes"))
--- R/grep-like_functions.R
+++ R/grep-like_functions.R
@@ -1,15 +1,22 @@
 
+#' @export
 grepi = function(...) grep(..., ignore.case=TRUE)
 
+#' @export
 grepv = function(...) grep(..., value=TRUE)
 
+#' @export
 grepvi = function(...) grep(..., value=TRUE, ignore.case=TRUE)
 
+#' @export
 `%like%` = function(x, y) grepl(y, x, ignore.case=TRUE)
 
+#' @export
 `%Like%` = function(x, y) grepl(y, x)
 
+#' @export
 `%unlike%` = function(x, y) !grepl(y, x, ignore.case=TRUE)
 
+#' @export
 `%Unlike%` = function(x, y) !grepl(y, x)
 
