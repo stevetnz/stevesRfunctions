5cdd75772e08ad022ec7f757fe875d4c97ed88bd
Steve Taylor <steve.taylor.nz@gmail.com>
2018-06-05 14:32:11 +1200

    Tweaks to NoYes function

--- R/factor_functions.R
+++ R/factor_functions.R
@@ -1,4 +1,4 @@
-#' Convert a logical vector to a factor
+#' Convert a logical vector to a two-level factor
 #'
 #' Converts a logical vector to a factor conatining two levels.
 #' This is useful in statistical models such as logistic regression,
@@ -7,7 +7,9 @@
 #' @param bool A logical vector, or a vector that can be coerced to logical.
 #' @param ifFALSE factor level for FALSE values, defaults to "No".
 #' @param ifTRUE factor level for TRUE values, defaults to "Yes".
+#' @value A factor with two levels.
 #' @examples
+#' table(NoYes(LETTERS %in% c('A','E','I','O','U')))
 #' table(NoYes(rnorm(100) > 0.5))
 #' @export
 NoYes = function(bool, ifFALSE='No', ifTRUE='Yes') {
